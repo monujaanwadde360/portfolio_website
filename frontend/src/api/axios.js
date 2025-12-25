b11925fc3b32f3d9329d853bb1bc8b7b6c1c000d
monujaanwadde360 <monujaanwadde360@gmail.com>
2025-12-25 22:57:59 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://portfolio-website-backend-77uc.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
